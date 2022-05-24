81663430f65d7b052a060a7850df6a8f6c2534e6
Amol Ghotale <ghotaleamol@gmail.com>
2022-05-24 11:36:01 +0530

    ADDED: Lombak Library for Logging

--- src/main/java/com/example/employeepayrollappspringboot/EmployeePayrollAppSpringBootApplication.java
+++ src/main/java/com/example/employeepayrollappspringboot/EmployeePayrollAppSpringBootApplication.java
@@ -1,14 +1,17 @@
 package com.example.employeepayrollappspringboot;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@Slf4j
 public class EmployeePayrollAppSpringBootApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(EmployeePayrollAppSpringBootApplication.class, args);
-        System.out.println("Welcome to Employee Payroll Application.");
-    }
+  public static void main(String[] args) {
+    SpringApplication.run(EmployeePayrollAppSpringBootApplication.class, args);
+    log.info("Employee Payroll Application Started!!!");
+    System.out.println("Welcome to Employee Payroll Application.");
+  }
 
 }
