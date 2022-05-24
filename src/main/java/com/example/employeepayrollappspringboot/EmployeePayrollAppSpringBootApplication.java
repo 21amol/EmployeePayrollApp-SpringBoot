b878d9a36e841e73d7dabbfc7dad2df22fc1dda9
Amol Ghotale <ghotaleamol@gmail.com>
2022-05-24 12:06:00 +0530

    ADDED: dev and prod  properties

--- src/main/java/com/example/employeepayrollappspringboot/EmployeePayrollAppSpringBootApplication.java
+++ src/main/java/com/example/employeepayrollappspringboot/EmployeePayrollAppSpringBootApplication.java
@@ -3,15 +3,16 @@ package com.example.employeepayrollappspringboot;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 @Slf4j
 public class EmployeePayrollAppSpringBootApplication {
 
   public static void main(String[] args) {
-    SpringApplication.run(EmployeePayrollAppSpringBootApplication.class, args);
-    log.info("Employee Payroll Application Started!!!");
+    ApplicationContext context = SpringApplication.run(EmployeePayrollAppSpringBootApplication.class, args);
+    log.info("Employee Payroll Application Started in {} Environment",
+            context.getEnvironment().getProperty("environment"));
     System.out.println("Welcome to Employee Payroll Application.");
   }
-
 }
